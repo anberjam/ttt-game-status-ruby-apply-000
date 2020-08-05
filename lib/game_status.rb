a7c8e99e2212d5f71ffc3f2241637f01f9ce2288
Anber Jamshed <anberjam@yahoo.com>
2020-08-05 03:06:02 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,77 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = 
+[
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+  ]
+
+
+  def won? (board)
+    if board.all?("") || board.all?(" ") || board.all?(nil)
+      return nil
+    end
+        
+    WIN_COMBINATIONS.each do |win_index|
+        if board[win_index[0]]=="X" && board[win_index[1]]=="X" && board[win_index[2]]=="X"
+            return win_index
+        elsif board[win_index[0]]=="O" && board[win_index[1]]=="O" && board[win_index[2]]=="O"
+            return win_index
+          end
+        end
+        if board.none?("") && board.none?(" ") && board.none?(nil)
+        return nil
+      end
+  end
+  
+  def full?(board)
+    if board.none?(" ") && board.none?("") && board.none?(nil)
+      return true
+    else
+      return false
+    end
+  end
+
+def draw? (board)
+  if won?(board) != nil
+    return false
+  end
+  if (board[0]==board[4] && board[4]==board[8]) || (board[2]==board[4] && board[4]==board[6])
+    return false
+  end
+  if full?(board) == true
+    return true
+  end
+end
+
+def over? (board)
+  if draw?(board)==true
+    return true
+  end
+  if (won?(board) != nil)
+    return true
+  end
+  if full?(board) == true
+  return true
+end
+end
+
+def winner(board)
+  if won?(board)==nil || draw?(board)
+    return nil
+  end
+  WIN_COMBINATIONS.each do |win_index|
+        if board[win_index[0]]=="X" && board[win_index[1]]=="X" && board[win_index[2]]=="X"
+            return "X"
+        elsif board[win_index[0]]=="O" && board[win_index[1]]=="O" && board[win_index[2]]=="O"
+            return "O"
+          end
+        end
+end
