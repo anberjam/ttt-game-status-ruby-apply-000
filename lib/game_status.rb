d3f181be33ac01d6024723ca5cd3e02734069c89
Anber Jamshed <anberjam@yahoo.com>
2020-08-05 02:20:51 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -26,7 +26,7 @@ WIN_COMBINATIONS =
             return win_index
         elsif board[win_index[0]]=="O" && board[win_index[1]]=="O" && board[win_index[2]]=="O"
             return win_index
-        elsif board[win_index[0]] != board[win_index[1]] || board[win_index[1]] != board[win_index[2]]
+        elsif board[win_index[0]] != board[win_index[1]] || board[win_index[1]] != board[win_index[2]] || board[win_index[2]] != board[win_index[0]]
         return nil
       end
     end
